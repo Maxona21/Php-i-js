8bf5b24fb42a0c3321eacb276decfac0ef37df27
Maxona21 <jessicajacion25@gmail.com>
2026-04-28 11:08:17 +0200

    Add files via upload

---
+++ PHP/smoki/bloki.js
@@ -0,0 +1,28 @@
+
+          
+            function blok1(){
+            
+                document.getElementById("nav1").style.backgroundColor= "MistyRose";
+                document.getElementById("nav2").style.backgroundColor= "#FFAEA5";
+                document.getElementById("nav3").style.backgroundColor= "#FFAEA5";
+                document.getElementById("sekcja1").style.display = "block";
+                document.getElementById("sekcja2").style.display = "none";
+                document.getElementById("sekcja3").style.display = "none";
+            }
+            function opisy(){
+                document.getElementById("nav2").style.backgroundColor= "MistyRose";
+                document.getElementById("nav1").style.backgroundColor= "#FFAEA5";
+                document.getElementById("nav3").style.backgroundColor= "#FFAEA5";
+                document.getElementById("sekcja2").style.display = "block";
+                document.getElementById("sekcja1").style.display = "none";
+                document.getElementById("sekcja3").style.display = "none";
+            }
+            function Galeria(){
+                document.getElementById("nav3").style.backgroundColor= "MistyRose";
+                document.getElementById("nav1").style.backgroundColor= "#FFAEA5";
+                document.getElementById("nav2").style.backgroundColor= "#FFAEA5";
+                document.getElementById("sekcja3").style.display = "block";
+                document.getElementById("sekcja1").style.display = "none";
+                document.getElementById("sekcja2").style.display = "none";
+            }
+           
